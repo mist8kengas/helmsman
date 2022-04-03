b8b939e338b01b0937a2ba63204f0c6e7dc1b28d
mist8kengas <touchgamer365@gmail.com>
2022-04-04 02:42:20 +0400

    [Fix] Added message on initial run

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
 import logger from './utils/logger.js';
 
-// import server
-import './server/index.js';
-
 // setup - check if ci.json exists
-import { existsSync, writeFileSync } from 'fs';
+import { existsSync, mkdirSync, writeFileSync } from 'fs';
+if (!existsSync('./var/config')) mkdirSync('./var/config', { recursive: true });
 if (!existsSync('./var/config/ci_cd.json')) {
     writeFileSync('./var/config/ci_cd.json', '{}');
     logger(
         `Created file "./var/config/ci_cd.json" because it does not exist`,
         'info'
     );
+    console.log(
+        '[helmsman]'.yellow,
+        'Please configure "./var/config/ci_cd.json" and run the program again.'
+    );
+    process.exit(1);
 }
+
+// import server
+import './server/index.js';
