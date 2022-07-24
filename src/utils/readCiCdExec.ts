b3b1b552a51f5c472351bc576467f80cc948e1d0
mist8kengas <touchgamer365@gmail.com>
2022-07-25 02:13:10 +0400

    [Fix] Added invalid syntax handler in readCiCdExec Referenced in issue #2

--- src/utils/readCiCdExec.ts
+++ src/utils/readCiCdExec.ts
@@ -1,10 +1,21 @@
 import { readFileSync } from 'fs';
+import logger from './logger.js';
 
 export default function readCiCdExec(
     repository: string,
     type: string
 ): string | undefined {
-    const jsonPath = './var/config/ci_cd.json',
+    const jsonPath = './var/config/ci_cd.json';
+    let ciCd = undefined;
+
+    try {
         ciCd = JSON.parse(readFileSync(jsonPath, { encoding: 'utf-8' }));
+    } catch (error) {
+        logger(`Invalid syntax detected in "./var/config/ci_cd.json"`, 'error');
+        const message =
+            'An error occured while trying to parse "./var/config/ci_cd.json", check for invalid syntax and run the program again. (Check error logs)';
+        console.log('[helmsman]'.red, message, error);
+        throw new Error(message);
+    }
     return ciCd[repository] ? ciCd[repository][type] : undefined;
 }
